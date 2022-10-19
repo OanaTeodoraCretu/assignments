781e69f314070ef650cb47c9a724b5cab0139703
Oana Cretu <oanateodora.cretu@lexisnexis.com>
2022-10-19 14:02:41 +0300

    initial setup

---
+++ schedules/src/test/java/com/geosatis/schedules/SchedulesApplicationTests.java
@@ -0,0 +1,8 @@
+package com.geosatis.schedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulesApplicationTests {
+}
